32ec15d10e083c498b44b324cc1cf19d901a4ee4
Devin <wangle521258@gmail.com>
2023-01-05 00:29:05 +1100

    mysql 环境配置完成

---
+++ src/main/java/com/example/ushops/UshopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ushops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UshopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UshopsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ushops/UshopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ushops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UshopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
