46a70b65c920a2a31265a52c37f1afe7ae7ff49e
Devin <wangle521258@gmail.com>
2023-01-06 00:18:35 +1100

    创建订单

---
+++ order-service/src/main/java/com/example/form/BuyerOrderForm.java
@@ -0,0 +1,17 @@
+package com.example.form;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class BuyerOrderForm {
+    private String name;
+    private String phone;
+    private String address;
+    private Integer id;
+
+    private List<BuyerOrderDetailForm> items;
+
+
+}
