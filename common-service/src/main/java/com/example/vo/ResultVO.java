61861fe8c59d581fe427a9148172a9e5fff0fa72
Devin <wangle521258@gmail.com>
2023-01-05 20:22:09 +1100

    product services  1/3 程度完成,可以返回数据但是需要修改数据

---
+++ common-service/src/main/java/com/example/vo/ResultVO.java
@@ -0,0 +1,9 @@
+package com.example.vo;
+
+import lombok.Data;
+@Data
+public class ResultVO<T> {
+    private Integer code;
+    private String msg;
+    private T data;
+}
---
+++ product-service/src/main/java/com/example/controller/BuyerProductController.java
@@ -0,0 +1,41 @@
+package com.example.controller;
+
+
+import com.example.service.ProductCategoryService;
+import com.example.vo.ResultVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 类目表 前端控制器
+ * </p>
+ *
+ * @author devin
+ * @since 2023-01-05
+ */
+@RestController
+@RequestMapping("/buyer/product")
+public class BuyerProductController {
+
+    @Autowired
+    private ProductCategoryService productCategoryService;
+
+    @GetMapping("/list")
+    public ResultVO list(){
+        ResultVO resultVO = new ResultVO<>();
+        resultVO.setCode(0);
+        resultVO.setMsg("success");
+        resultVO.setData(this.productCategoryService.list());
+        return resultVO;
+
+    }
+
+}
+
