5af6798b8978100a5b1bf339846dc986c101ece1
Devin <wangle521258@gmail.com>
2023-01-15 23:47:21 +1100

    注册手机号验证,登录的部分验证

--- account-service/src/main/java/com/example/controller/UserController.java
+++ account-service/src/main/java/com/example/controller/UserController.java
@@ -1,19 +1,21 @@
 package com.example.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.entity.User;
 import com.example.exception.ShopException;
 import com.example.form.UserForm;
 import com.example.result.ResponseEnum;
+import com.example.service.UserService;
+import com.example.util.MD5Util;
 import com.example.util.RegexValidateUtil;
 import com.example.util.ResultVOUtil;
 import com.example.vo.ResultVO;
 import net.bytebuddy.implementation.bytecode.Throw;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -26,15 +28,51 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
     @PostMapping("/register")
     public ResultVO register(@RequestBody UserForm userForm){
+        //验证手机号
         boolean b = RegexValidateUtil.checkMobile(userForm.getMobile());
         if (!b){
             throw new ShopException(ResponseEnum.MOBILE_ERROR.getMsg() );
 
         }
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("mobile",userForm.getMobile());
+        User one = this.userService.getOne(queryWrapper);
+        if (one != null){
+            throw new ShopException(ResponseEnum.MOBILE_EXIST.getMsg());
+
+        }
+        //校验
+        User user = new User();
+        user.setMobile(userForm.getMobile());
+        user.setPassword(MD5Util.getSaltMD5(userForm.getPassword()));
+        this.userService.save(user);
         return ResultVOUtil.success(null);
 
+    }
+
+    @GetMapping("/login")
+    public ResultVO login(UserForm userForm){
+        boolean b = RegexValidateUtil.checkMobile(userForm.getMobile());
+        if (!b){
+            throw new ShopException(ResponseEnum.MOBILE_ERROR.getMsg() );
+
+        }
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("mobile",userForm.getMobile());
+        User one = this.userService.getOne(queryWrapper);
+        if (one == null){
+            throw new ShopException(ResponseEnum.MOBILE_IS_NULL.getMsg());
+        }
+        //密码验证
+        if (! MD5Util.getSaltverifyMD5(userForm.getPassword(),one.getPassword())){
+            throw new ShopException(ResponseEnum.PASSWORD_ERROR.getMsg());
+
+        }
+        return ResultVOUtil.success(null);
     }
 
 }
--- common-service/src/main/java/com/example/result/ResponseEnum.java
+++ common-service/src/main/java/com/example/result/ResponseEnum.java
@@ -8,7 +8,10 @@ import lombok.Getter;
 public enum ResponseEnum {
 
     STOCK_ERROR(300,"库存不足"),
-    MOBILE_ERROR(301, "手机格式有误");
+    MOBILE_ERROR(301, "手机格式有误"),
+    MOBILE_EXIST(302,"手机号已存在"),
+    MOBILE_IS_NULL(303,"手机号未注册"),
+    PASSWORD_ERROR(304,"密码错误");
 
     private Integer code;
     private String msg;
